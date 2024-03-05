e1f082a9c448eaae46f640baa929a9019e4534d0
frokanic <f.rokanis@gmail.com>
2024-03-05 20:32:03 +0200

    1. Finalized the searchbar 2. Created the circle that I will use in my compass project

--- app/src/main/java/com/projects/composables/components/bar/SearchBar.kt
+++ app/src/main/java/com/projects/composables/components/bar/SearchBar.kt
@@ -4,45 +4,61 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MySearchBar() {
 
+    var query by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
     SearchBar(
-        query = "",
-        onQueryChange = {},
-        onSearch = {},
-        active = true,
-        onActiveChange = {},
-        placeholder = { Text(text = "Search...") },
+        query = query,
+        onQueryChange = { query = it },
+        onSearch = { /* What action do you wish to be performed upon searching */ },
+        active = active,
+        onActiveChange = { active = it },
+        placeholder = {
+            Text("Search...")
+        },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = "Search Icon"
             )
         },
-        trailingIcon = {
-
-        }
+        trailingIcon = if (active) {
+            {
+                IconButton(
+                    onClick = {
+                        if (query.isNotEmpty()) query = "" else active = false
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close Search Button"
+                    )
+                }
+            }
+        } else null
     ) {
 
     }
-//    TextField(
-//        value = "",
-//        onValueChange = {},
-//        modifier = Modifier.fillMaxWidth(),
-//        placeholder = { Text(text = "Search...") }
-//    )
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/projects/composables/components/shape/circle/LineCircle.kt
+++ app/src/main/java/com/projects/composables/components/shape/circle/LineCircle.kt
@@ -2,12 +2,17 @@ package com.projects.composables.components.shape.circle
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun LineCircle(
@@ -15,14 +20,38 @@ fun LineCircle(
 ) {
     Canvas(
         modifier = Modifier
-            .padding(20.dp)
             .fillMaxSize()
+            .size(200.dp)
     ) {
 
+        for (i in 0..179) {
+            /**
+             * radians = degrees * (PI / 180)
+             */
+            val angleInRad = i * 2f * (PI.toFloat() /  180f) + 270f
+            val lineLength = if (i % 10 == 0) 6.dp.toPx() else 3.dp.toPx()
+            val strokeWidth = if (i % 10 == 0) 1.5.dp.toPx() else 0.7.dp.toPx()
+
+            val lineStart = Offset(
+                x = (radius.toPx() + lineLength) * cos(angleInRad) + center.x,
+                y = (radius.toPx() + lineLength) * sin(angleInRad) + center.y
+            )
+            val lineEnd = Offset(
+                x = (radius.toPx() - lineLength) * cos(angleInRad) + center.x,
+                y = (radius.toPx() - lineLength) * sin(angleInRad) + center.y
+            )
+
+            drawLine(
+                color = Color.Black,
+                start = lineStart,
+                end = lineEnd,
+                strokeWidth = strokeWidth
+            )
+        }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun LineCirclePreview() {
     LineCircle(100.dp)
